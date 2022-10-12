5e3de21a11165944b5b469ab38cb287f67787538
myTerminal <team.terminal@gmail.com>
2022-10-11 21:57:53 -0500

    implement notifications with 'notify-send'

--- src/main.lisp
+++ src/main.lisp
@@ -10,6 +10,8 @@
         (log-to-stdout "Power below set threshold! Suspending...")
         (if (exists-in-system-p "beep")
             (execute-in-system "beep -l 500"))
+        (if (exists-in-system-p "notify-send")
+            (execute-in-system "notify-send \"Power below set threshold!\" \"The system will now be suspended.\""))
         (suspend-system))
       (log-to-stdout "Power looks OK.")))
 
