eacb567140a3c79f2bfec204f24ce9232aaafc38
myTerminal <team.terminal@gmail.com>
2022-10-16 12:52:07 -0500

    implement adaptable sleep timer to check battery level

--- src/main.lisp
+++ src/main.lisp
@@ -3,9 +3,9 @@
 
 (in-package :main)
 
-(defun run-check (battery-threshold)
+(defun run-check (current-battery-charge battery-threshold)
   "Runs the monitoring routine."
-  (if (< (get-remaining-charge) (parse-integer battery-threshold))
+  (if (< current-battery-charge battery-threshold)
       (progn
         (execute-if-exists "beep" "-l 500")
         (log-to-user "Power below set threshold! The system will now be suspended.")
@@ -15,8 +15,29 @@
 (defun main ()
   "The main entry point to the program."
   (let* ((args (uiop:command-line-arguments))
-         (battery-threshold (or (car args)
-                                "10")))
-    (loop
-     (run-check battery-threshold)
-     (sleep 20))))
+         (battery-threshold (parse-integer (or (car args)
+                                               "10")))
+         (current-battery-level 50)
+         (previous-battery-level 50)
+         (sleep-timer 15))
+    (labels ((fetch-battery-level ()
+               (setf current-battery-level
+                     (get-remaining-charge))) ;; Stores the current battery-level
+             (adjust-timer ()
+               ;; Check if the level has dropped
+               (if (< current-battery-level previous-battery-level)
+                   (and (setf sleep-timer 15)) ; Speed back timer
+                   (and (< sleep-timer 60) (setf sleep-timer
+                                                 (+ sleep-timer 5)))))) ; Slow down timer
+      (loop
+       (fetch-battery-level) ; Poll for remaining battery charge
+       (adjust-timer) ; Adapt timer according to the current level
+       (run-check current-battery-level
+                  battery-threshold) ; Run check
+       (setf previous-battery-level current-battery-level) ; Store back current level
+       (log-to-stdout (concatenate 'string
+                                   "Will check after "
+                                   (write-to-string sleep-timer)
+                                   " seconds..."))
+       ;; Sleep for a while
+       (sleep sleep-timer)))))
