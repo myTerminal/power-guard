3edb9bdc9d3d09a0256e175c0eeec1af3f64f30b
myTerminal <team.terminal@gmail.com>
2023-12-12 21:20:57 -0600

    shorten variable names in main

--- src/main.lisp
+++ src/main.lisp
@@ -15,19 +15,19 @@
 
     ;; Proceed with the available batteries
     (let* ((args (uiop:command-line-arguments))
-           (battery-threshold (parse-integer (or (car args)
-                                                 "10")))
-           (current-battery-level 50)
-           (previous-battery-level 50)
+           (charge-threshold (parse-integer (or (car args)
+                                                "10")))
+           (current-charge 50)
+           (previous-charge 50)
            (sleep-delay 15))
-      (labels ((get-new-sleep-delay (sleep-delay current-battery-level previous-battery-level)
-                 (if (< current-battery-level previous-battery-level)
-                     15 ; Speed back timer
-                     (if (< sleep-delay 60)
-                         (+ sleep-delay 5)
-                         sleep-delay))) ; Slow down timer
-               (run-check (batteries current-battery-charge battery-threshold)
-                 (if (and (< current-battery-charge battery-threshold)
+      (labels ((get-new-sleep-delay (delay current-charge previous-charge)
+                 (if (< current-charge previous-charge)
+                     15 ; Drop delay
+                     (if (< delay 60)
+                         (+ delay 5)
+                         delay))) ; Increase delay
+               (run-check (batteries current-charge charge-threshold)
+                 (if (and (< current-charge charge-threshold)
                           (not (ac-power-connected-p)))
                      (if (cdr batteries)
                          (hibernate-system)
@@ -35,19 +35,19 @@
                      (log-to-system "Power looks OK."))))
         (loop
          ;; Read remaining battery charge
-         (setf current-battery-level
+         (setf current-charge
                (get-remaining-charge))
          ;; Perform check
          (run-check batteries
-                    current-battery-level
-                    battery-threshold)
+                    current-charge
+                    charge-threshold)
          ;; Adapt timer according to the current level
          (setf sleep-delay
                (get-new-sleep-delay sleep-delay
-                                    current-battery-level
-                                    previous-battery-level))
+                                    current-charge
+                                    previous-charge))
          ;; Record current battery level for future reference
-         (setf previous-battery-level current-battery-level)
+         (setf previous-charge current-charge)
          ;; Log about the next check
          (log-to-system (concatenate 'string
                                      "Will check after "
